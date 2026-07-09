bb006ebcfc18d6b21f6bb074151cc1e974ad7e92
Vishal Tomar <tomarvishal473@gmail.com>
2026-07-09 13:08:44 +0530

    Production deployment updates

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,12 +1,20 @@
 const mysql = require("mysql2");
+const fs = require("fs");
+const path = require("path");
 require("dotenv").config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+
+    ssl: {
+        ca: fs.readFileSync(
+            path.join(__dirname, "../../certs/ca.pem")
+        ),
+    },
 });
 
 module.exports = pool.promise();
